d76ec288dba821a94c90109c8518d336ecc9dba1
Julian Calzada <juliancalzada@gmail.com>
2022-06-24 17:32:01 -0500

    chore: adds error handling to api

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,20 +6,24 @@ import {
 } from "./_DATA";
 
 export const getInitialData = () => {
-  return Promise.all([_getUsers(), _getQuestions()]).then(
-    ([users, questions]) => ({
+  return Promise.all([_getUsers(), _getQuestions()])
+    .then(([users, questions]) => ({
       users,
       questions,
-    })
-  );
+    }))
+    .catch((e) => {
+      console.error("An error occurred with [getInitialData]: ", e);
+    });
 };
 
 export const saveQuestion = (question) => {
-  return _saveQuestion(question);
+  return _saveQuestion(question).catch((e) => {
+    console.error("An error occurred with [saveQuestion]: ", e);
+  });
 };
 
 export const saveQuestionAnswer = (authedUser, qid, answer) => {
   return _saveQuestionAnswer({ authedUser, qid, answer }).catch((e) => {
-    console.error("An error occurred with [_saveQuestionAnswer]: ", e);
+    console.error("An error occurred with [saveQuestionAnswer]: ", e);
   });
 };
