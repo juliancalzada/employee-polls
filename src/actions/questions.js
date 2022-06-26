ba261ee4141dfe70a6092cb6534e8392626def17
Julian Calzada <juliancalzada@gmail.com>
2022-06-26 11:19:50 -0500

    feat: allows questions to save votes

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,9 +1,30 @@
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
 export const SAVE_VOTE = "SAVE_VOTE";
 
+/**
+ * Loads the questions database.
+ * @param {object} questions The `questions` database
+ * @returns {object}
+ */
 export const receiveQuestions = (questions) => {
   return {
     type: RECEIVE_QUESTIONS,
     questions,
+  };
+};
+
+/**
+ * Records the user's vote on the current poll question.
+ * @param {string} authedUser The id of the authorized user
+ * @param {string} qid The id of the current poll question
+ * @param {string} answer The id of the selected answer
+ * @returns {object}
+ */
+export const saveVote = (authedUser, qid, answer) => {
+  return {
+    type: SAVE_VOTE,
+    authedUser,
+    qid,
+    answer,
   };
 };
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,7 +1,6 @@
-import { RECEIVE_QUESTIONS } from "../actions/questions";
+import { RECEIVE_QUESTIONS, SAVE_VOTE } from "../actions/questions";
 
 export default function questions(state = {}, action) {
-
   switch (action.type) {
     case RECEIVE_QUESTIONS:
       return {
@@ -9,20 +8,17 @@ export default function questions(state = {}, action) {
         ...action.questions,
       };
 
-    // case SAVE_QUESTION_ANSWER:
-    //   return {
-    //     ...state,
-    //     users: {
-    //       ...state.users,
-    //       [action.authedUser]: {
-    //         ...state.users[action.authedUser],
-    //         answers: {
-    //           ...state.users[action.authedUser].answer,
-    //           [action.qid]: action.answer,
-    //         },
-    //       },
-    //     },
-    //   };
+    case SAVE_VOTE:
+      return {
+        ...state,
+        [action.qid]: {
+          ...state[action.qid],
+          [action.answer]: {
+            ...state[action.qid][action.answer],
+            votes: [action.authedUser].concat(state[action.qid][action.answer].votes),
+          },
+        },
+      };
 
     default:
       return state;
