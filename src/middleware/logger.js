c67bfeffebe2b83f1de4575dbc2e6ad347999da1
Julian Calzada <juliancalzada@gmail.com>
2022-06-26 10:05:15 -0500

    chore: logger output adjustments

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,7 +1,8 @@
 const logger = (store) => (next) => (action) => {
   console.group(action.type);
   const retVal = next(action);
-  console.log("The new state: ", store.getState());
+  console.log("The new state");
+  console.dir(store.getState());
   console.table(action);
   console.groupEnd();
   return retVal;
