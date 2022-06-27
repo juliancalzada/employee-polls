616ff11a01687e0cea3f52c27efd86b1307d51ef
Julian Calzada <juliancalzada@gmail.com>
2022-06-26 21:50:29 -0500

    refactor: adds details

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,9 +1,10 @@
 const logger = (store) => (next) => (action) => {
   console.group(action.type);
   const retVal = next(action);
-  console.log("The new state");
-  console.dir(store.getState());
+  console.log("The current action");
   console.table(action);
+  console.log("The new state");
+  console.table(store.getState());
   console.groupEnd();
   return retVal;
 };
