969f81e9a7897aeecb5352bc0b740504dac22e41
Julian Calzada <juliancalzada@gmail.com>
2022-06-30 02:25:10 -0500

    chore: removes unused var

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { handleSaveQuestionAnswer } from "../actions/shared";
 import { withRouter } from "../utils/helpers";
