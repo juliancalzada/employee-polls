f5a4750cbc779e6347271c18925e29890b492165
Julian Calzada <juliancalzada@gmail.com>
2022-07-03 16:35:37 -0500

    fix: re-orders tests to prevent jest error

--- src/utils/_DATA.test.js
+++ src/utils/_DATA.test.js
@@ -1,12 +1,6 @@
 import { _saveQuestion, _saveQuestionAnswer } from "./_DATA";
 
 describe("_saveQuestion", () => {
-  it("will return an error", async () => {
-    await expect(_saveQuestion({})).rejects.toEqual(
-      "Please provide optionOneText, optionTwoText, and author"
-    );
-  });
-
   it("will save and return a formatted question", async () => {
     const author = "author_id";
     const optionOneText = "option_one_text";
@@ -15,32 +9,36 @@ describe("_saveQuestion", () => {
     const expectation = {
       author: author,
       optionOne: {
-        votes: [],
         text: optionOneText,
       },
       optionTwo: {
-        votes: [],
         text: optionTwoText,
       },
     };
+
     await expect(_saveQuestion(question)).resolves.toMatchObject(expectation);
+  });
+
+  it("will return an error", async () => {
+    await expect(_saveQuestion({})).rejects.toEqual(
+      "Please provide optionOneText, optionTwoText, and author"
+    );
   });
 });
 
 describe("_saveQuestionAnswer", () => {
+  it("will save the answer to the question and return true", async () => {
+    const authedUser = "zoshikanlu";
+    const qid = "8xf0y6ziyjabvozdd253nd";
+    const answer = "optionTwo";
+    const update = { authedUser, qid, answer };
+
+    await expect(_saveQuestionAnswer(update)).resolves.toBe(true);
+  });
+
   it("will return an error", async () => {
     await expect(_saveQuestionAnswer({})).rejects.toEqual(
       "Please provide authedUser, qid, and answer"
     );
-  });
-
-  it("will save the answer to the questions and returns true", async () => {
-    const authedUser = "zoshikanlu";
-    const qid = "8xf0y6ziyjabvozdd253nd";
-    const answer = "optionTwo";
-
-    await expect(
-      _saveQuestionAnswer({ authedUser, qid, answer })
-    ).resolves.toBe(true);
   });
 });
