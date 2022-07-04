a39174fac0c672852717b5cd130c037bd130d416
Julian Calzada <juliancalzada@gmail.com>
2022-07-04 15:00:09 -0500

    chore: adds tests to SignOut

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -2,37 +2,37 @@ import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import { setAuthedUser } from "../actions/authedUser";
 
-const SignOut = (props) => {
-  const { authedUser, dispatch } = props;
+const SignOut = ({ name, avatarURL, dispatch }) => {
   const handleSignOut = (e) => {
     e.preventDefault();
     dispatch(setAuthedUser(null));
   };
 
   return (
-    <Fragment>
-      <div className="columns is-mobile">
-        <div className="column is-narrow">
-          <figure>
-            <img src={authedUser.avatarURL} alt={authedUser.name} />
-          </figure>
-        </div>
-        <div className="column">
-          <p className="title is-6">{authedUser.name}</p>
-          <p className="subtitle is-7">
-            <a href="/" onClick={handleSignOut}>
-              sign out
-            </a>
-          </p>
-        </div>
-      </div>
-    </Fragment>
+<Fragment>
+  <div className="columns is-mobile">
+    <div className="column is-narrow">
+      <figure>
+        <img src={avatarURL} alt={name} />
+      </figure>
+    </div>
+    <div className="column">
+      <p className="title is-6">{name}</p>
+      <p className="subtitle is-7">
+        <button className="button is-small" onClick={handleSignOut}>sign out</button>
+      </p>
+    </div>
+  </div>
+</Fragment>
   );
 };
 
 const mapStateToProps = ({ authedUser, users }) => {
+  const { name, avatarURL } = users[authedUser];
+
   return {
-    authedUser: users[authedUser],
+    name,
+    avatarURL,
   };
 };
 
---
+++ src/components/SignOut.test.js
@@ -0,0 +1,48 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import reducer from "../reducers";
+import SignOut from "./SignOut";
+const mockStore = configureStore([thunk]);
+
+describe("SignOut", () => {
+  const NAME = "Sarah Edo";
+  const AUTHED_USER = "sarahedo";
+  let store, component;
+
+  beforeEach(() => {
+    store = mockStore({
+      authedUser: AUTHED_USER,
+      users: {
+        [AUTHED_USER]: {
+          id: AUTHED_USER,
+          name: NAME,
+          avatarURL: "https://placekitten.com/g/240/240",
+        },
+      },
+    });
+
+    store.replaceReducer(reducer);
+
+    component = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <SignOut />
+        </BrowserRouter>
+      </Provider>
+    );
+  });
+
+  it("should match the snapshot", () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should sign out the current user", () => {
+    const button = screen.getByText(/sign out/);
+    fireEvent.click(button);
+    const { authedUser } = store.getState();
+    expect(authedUser).toBeNull();
+  });
+});
